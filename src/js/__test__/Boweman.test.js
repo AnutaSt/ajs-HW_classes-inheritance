af5e399ed35f5cbd1e34f766c778b46734df2bef
AnnaSt <anutabatuk@gmail.com>
2023-03-09 14:55:38 +0800

    first version

---
+++ src/js/__test__/Boweman.test.js
@@ -0,0 +1,36 @@
+import Boweman from '../Boweman';
+
+test('check boweman is created', () => {
+  const newBw = new Boweman('gamer', 'Zombie');
+  expect(newBw).toEqual(
+    {
+      name: 'gamer',
+      type: 'Boweman',
+      health: 100,
+      level: 1,
+      attack: 25,
+      defence: 25,
+    },
+  );
+});
+
+test('check boweman func levelUp', () => {
+  const newBw = new Boweman('gamer', 'Boweman');
+  newBw.levelUp();
+  expect(newBw).toEqual(
+    {
+      name: 'gamer',
+      type: 'Boweman',
+      health: 100,
+      level: 2,
+      attack: 30,
+      defence: 30,
+    },
+  );
+});
+
+test('check Boweman  func damage', () => {
+  const newBw = new Boweman('gamer', 'Boweman');
+  newBw.damage(30);
+  expect(newBw.health).toBe(77.5);
+});
